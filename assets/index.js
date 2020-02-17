114b5914b6268e724c5ad7ced460c7046fe1ac76
Dylan Collins <dcollins5590@students.mc3.edu>
2020-02-17 16:30:35 -0500

    finished the nav bar on the home page

---
+++ assets/index.js
@@ -0,0 +1,16 @@
+//creating a variable for the anchor elements 
+const anchorTags = document.querySelectorAll('a');
+//looping through the anchor elements so they underline/upscale when the mouse goes over them
+anchorTags.forEach( item => item.addEventListener('mouseover', function (){
+ item.style.textDecoration = 'underline';
+ item.style.transform = 'scale(1.3)';
+ item.style.transition = 'transform .3s';
+}))
+//looping back over the elements to take off the text decoration/descale when the mouse leaves
+anchorTags.forEach( item => item.addEventListener('mouseleave', function (){
+    item.style.textDecoration = 'none';
+    item.style.transform = 'scale(1.0)';
+    item.style.transition = 'transform .3s';
+}))
+
+
