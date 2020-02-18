5bfd35375dccc60655f788fe31dba29fad80db12
Dylan Collins <dcollins5590@students.mc3.edu>
2020-02-18 11:09:58 -0500

    got my photo on the landing page and the button component in

--- assets/index.js
+++ assets/index.js
@@ -1,16 +1,51 @@
 //creating a variable for the anchor elements 
 const anchorTags = document.querySelectorAll('a');
+
 //looping through the anchor elements so they underline/upscale when the mouse goes over them
 anchorTags.forEach( item => item.addEventListener('mouseover', function (){
  item.style.textDecoration = 'underline';
  item.style.transform = 'scale(1.3)';
  item.style.transition = 'transform .3s';
 }))
+
 //looping back over the elements to take off the text decoration/descale when the mouse leaves
 anchorTags.forEach( item => item.addEventListener('mouseleave', function (){
     item.style.textDecoration = 'none';
     item.style.transform = 'scale(1.0)';
     item.style.transition = 'transform .3s';
 }))
+
+//creating variable of div element to insert my button into 
+const container = document.querySelector('.body')
+
+//creating a buttton component 
+function buttonCreator (text, url){
+    const button = document.createElement('div');
+    const anchor = document.createElement('a');
+    button.classList.add('projects-button');
+    const span = document.createElement('span');
+    span.classList.add('transparent');
+    button.appendChild(anchor);
+    anchor.appendChild(span);
+    span.textContent = text; 
+    anchor.href = url;
+    anchor.style.textDecoration = "none";
+    button.style.opacity= '.8'
+
+    button.addEventListener('mouseover', ()=> {
+        button.style.transform = 'scale(1.3)'; 
+        button.style.transition = 'transform .5s';
+        button.style.opacity = '5';
+    })
+
+    button.addEventListener('mouseleave', ()=> {
+        button.style.transform = 'scale(1.0)';
+        button.style.opacity = '.8';
+    })
+    return button
+}
+
+//appending child to the the body class 
+container.appendChild(buttonCreator('View Projects','../projects.html'));
 
 
